0292663e1bd3f9b9e9ace338b4218b7eb159938c
Appiah Collins <appiahc86@gmail.com>
2021-12-10 07:20:58 +0000

    fixing

--- app.js
+++ app.js
@@ -31,15 +31,15 @@ app.use((req, res, next) => {
     // res.header('Access-Control-Allow-Origin', '*');
     res.set('Access-Control-Allow-Origin', '*');
 
-    if (req.method === 'OPTIONS') {
+    // if (req.method === 'OPTIONS') {
         // Send response to OPTIONS requests
-        res.set('Access-Control-Allow-Methods', 'GET');
-        res.set('Access-Control-Allow-Headers', 'Content-Type');
-        res.set('Access-Control-Max-Age', '3600');
-        res.status(204).send('');
-    } else {
-        res.send('Hello World!');
-    }
+    //     res.set('Access-Control-Allow-Methods', 'GET');
+    //     res.set('Access-Control-Allow-Headers', 'Content-Type');
+    //     res.set('Access-Control-Max-Age', '3600');
+    //     res.status(204).send('');
+    // } else {
+    //     res.send('Hello World!');
+    // }
     next();
 });
 
