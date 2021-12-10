bc495549dc67af2a92e0b9bca8d8a0c6d7331ab0
Appiah Collins <appiahc86@gmail.com>
2021-12-10 07:17:07 +0000

    fixing

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 import cors from "cors"
 
 const app = express();
-
+app.use(cors());
 // app.use(morgan());
 app.use(express.json());
-app.use(cors());
+
 
 //ENV
 dotenv.config();
@@ -28,7 +28,18 @@ mongoose.connect(process.env.DB_CONNECTION,
 // const port = process.env.port || 3000;
 
 app.use((req, res, next) => {
-    req.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Origin', '*');
+
+    if (req.method === 'OPTIONS') {
+        // Send response to OPTIONS requests
+        res.set('Access-Control-Allow-Methods', 'GET');
+        res.set('Access-Control-Allow-Headers', 'Content-Type');
+        res.set('Access-Control-Max-Age', '3600');
+        res.status(204).send('');
+    } else {
+        res.send('Hello World!');
+    }
     next();
 });
 
