54f1c5301bd2c9e14c5f9aca151b72f4c7b39346
Appiah Collins <appiahc86@gmail.com>
2021-12-10 13:19:37 +0000

    fixing

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ import dotenv from "dotenv";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 
 // app.all('*', function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
